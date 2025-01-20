17f3aaf0bbaf611862d00f415506392f613667a8
Aldana Tamara Di Flavio <adiflavio@unlam.edu.ar>
2025-01-20 15:04:44 -0300

    se agrego framework tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+//esto es un componente
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          El conteo es {count}
-        </button>
-        <p>
-          Edite <code>src/App.jsx</code> y guárdelo para probar HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Haga clic en los logotipos de Vite y React para obtener más información
-      </p>
-    </>
+    <div>hola mundo</div>
   )
 }
 
 export default App
+
+/**
+ * COMPONENTE
+ * 
+ */
